69d7c5c40d859b1b945ddfd7b3a819829d4b2eff
KaleChips22 <jonasgarmon@gmail.com>
2023-07-31 10:08:50 -0400

    further work on multiplayer mode

--- online/host/script.js
+++ online/host/script.js
@@ -1,8 +1,13 @@
 let GAME_ID
 
-fetch('https://amongusserver.xnor.repl.co/newgame', {
-    headers: {
-        Accept: 'application/json'
-    }
-}).then(response => response.text())
-    .then(text => console.log(text))
+// fetch('https://amongusserver.xnor.repl.co/newgame', {
+//     headers: {
+//         Accept: 'application/json'
+//     }
+// }).then(response => response.text())
+//     .then(text => console.log(text))
+
+const req = new XMLHttpRequest()
+req.addEventListener('load', () => console.log(this.responseText))
+req.open('GET', 'https://amongusserver.xnor.repl.co/newgame')
+req.send()
